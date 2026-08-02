d0b90f3b092dc2f8b98a1b778127993028a96631
FARDeen9307 <FARDEENKHAN8411@GMAIL.COM>
2026-08-02 18:06:59 +0530

    simple GEMM done

--- assignment_01/src/gemm.cpp
+++ assignment_01/src/gemm.cpp
@@ -4,3 +4,25 @@
 #include  <string>
 #include  <chrono>
 #include  <algorithm>
+
+using namespace std;
+
+//simple GEMM
+
+void simpleGEMM(int m,int k, int n,
+                const vector<vector<int>>& matA,
+               const vector<vector<int>>& matB,
+               vector<vector<int>>& resC)  {
+
+                for(int i=0; i<m; i++){
+
+                    for(int j=0; j<n; j++) {
+                        resC[i][j]= 0;
+                        for(int p=0; p<k; p++) {
+
+                            resC[i][j]= resC[i][j] + matA[i][p] * matB[p][j];
+                        }
+                    }
+                }
+                
+               }
